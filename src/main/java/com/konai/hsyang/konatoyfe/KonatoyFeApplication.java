6799febe77da9878caedbcb8929d1d302ae410ba
hyunseung yang <dev.hsyang@gmail.com>
2022-10-20 13:36:22 +0900

    Initial commit

---
+++ src/main/java/com/konai/hsyang/konatoyfe/KonatoyFeApplication.java
@@ -0,0 +1,13 @@
+package com.konai.hsyang.konatoyfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonatoyFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonatoyFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konai/hsyang/konatoyfe/KonatoyFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konai.hsyang.konatoyfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonatoyFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
